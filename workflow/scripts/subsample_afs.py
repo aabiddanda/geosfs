22a5dd6329978aae52221036acb7c577e06c750a
Arjun Biddanda <aabiddanda@gmail.com>
2023-07-18 12:28:14 -0400

    feat: small update per-chromosome for SFS calculations

---
+++ workflow/scripts/subsample_afs.py
@@ -0,0 +1,15 @@
+#!python3
+
+"""Subsampling allele frequencies across populations from large catalogs."""
+
+import numpy as np 
+import pandas as pd
+
+def pop_spread(n=100, n_pop):
+    """Determine the number of individuals to sample. """
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
